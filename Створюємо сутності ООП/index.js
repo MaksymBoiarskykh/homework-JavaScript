2ea93ab4d0b6eb996e54f96f31712a759506c1e1
Maksym Boiarskykh <Boarskihmaks18@gmail.com>
2023-05-17 21:44:28 +0300

    homework oop 2

--- Створюємо сутності ООП/index.js
+++ Створюємо сутності ООП/index.js
@@ -1,10 +1,38 @@
 class Human {
   constructor(nameHuman, ageHuman) {
-    this.nameHuman = nameHuman;
-    this.ageHuman = ageHuman;
+    this.nameHuman = nameHuman || "unknown";
+    this.ageHuman = ageHuman || "unknown";
   }
-  info() {
-    console.log(`Human name is - ${nameHuman} and age - ${ageHuman}`);
+  getInfo() {
+    console.log(`Human name is - ${this.nameHuman} and age - ${this.ageHuman}`);
   }
 }
-class user1 = new Human("Max", 19)
+
+class Car {
+  constructor(brand, model, yearOfIssue, numberplate) {
+    this.brand = brand || "unknown";
+    this.model = model || "unknown";
+    this.yearOfIssue = yearOfIssue || "unknown";
+    this.numberplate = numberplate || "unknown";
+    this.owner = "unknown";
+  }
+
+  createOwner(human) {
+    if (human.ageHuman < 18) {
+      console.log("you are too young for driving");
+      return;
+    }
+    this.owner = human;
+  }
+}
+
+const firstUser = new Human("Julia", 33);
+firstUser.getInfo();
+
+const nissan = new Car("nissan", "leaf", 2013, 1540);
+nissan.createOwner(new Human("Anna", 20));
+console.log(nissan);
+
+const audi = new Car("audi", "S1", 2013, 3091);
+audi.createOwner(new Human("Max", 19));
+console.log(audi);
