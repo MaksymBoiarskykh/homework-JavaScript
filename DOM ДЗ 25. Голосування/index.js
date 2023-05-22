05761f8e793eb06c6cd13dddba1defbfb1f9073f
Maksym Boiarskykh <Boarskihmaks18@gmail.com>
2023-05-22 23:59:49 +0300

    hw25

---
+++ DOM ДЗ 25. Голосування/index.js
@@ -0,0 +1,18 @@
+const emogjiList = document.querySelectorAll(".emoji");
+
+function createCounter(votingArr) {
+  votingArr.forEach((item) => {
+    item.append(document.createElement("div"));
+  });
+
+  let numberList = document.querySelectorAll("div");
+
+  votingArr.forEach((item, index) => {
+    let count = 0;
+    item.addEventListener("click", () => {
+      numberList[index].innerHTML = ++count;
+      item.append(numberList[index]);
+    });
+  });
+}
+console.log(createCounter(emogjiList));
